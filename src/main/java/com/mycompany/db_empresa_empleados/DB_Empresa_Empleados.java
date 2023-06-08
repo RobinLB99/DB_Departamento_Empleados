ff5096aa091b820aae98a3ed632a2f4570b8f2e1
RobinLB99 <rjlb991102@hotmail.com>
2023-06-08 18:37:58 -0500

    Update DB_Empresa_Empleados.java

--- src/main/java/com/mycompany/db_empresa_empleados/DB_Empresa_Empleados.java
+++ src/main/java/com/mycompany/db_empresa_empleados/DB_Empresa_Empleados.java
@@ -1,31 +1,13 @@
 package com.mycompany.db_empresa_empleados;
 
-import com.mycompany.db_empresa_empleados.logica.Direccion;
-import com.mycompany.db_empresa_empleados.logica.Empleado;
-import com.mycompany.db_empresa_empleados.logica.LogicController;
-import java.util.Date;
+import com.mycompany.db_empresa_empleados.prompt.Comandline;
 
 public class DB_Empresa_Empleados {
 
     public static void main(String[] args) {
         
-        LogicController control = new LogicController();
-        
-        Direccion dir = new Direccion();
-        control.crearDireccion(dir);
-        
-        Empleado employ1 = new Empleado(
-                1, "Robin Joel", "Lugo Boero", new Date(),dir);
-        control.crearEmpleado(employ1);
-        
-        dir.setId(1);
-        dir.setEstado("Guayas");
-        dir.setCiudad("Guayaquil");
-        dir.setCalle("Av. Las Americas");
-        dir.setCodigoPostal("0955412");
-        
-        control.editarDireccion(dir);
-        
+        Comandline comandos = new Comandline();
+        comandos.iniciar();
         
     }
 }
